189349dddfa92a2c7956734b1872fe4479d6e1c9
YelLin21 <u6520242@au.edu>
2024-09-01 19:46:35 +0700

    Add APIs

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <h1>{process.env.COMPANY_NAME}</h1>
+      <p>This is the about page</p>
+    </main>
+  );
+}
+ 
